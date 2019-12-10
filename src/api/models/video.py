2caccb0186175fdfea35a3f0cd8a0cd9bed0ee45
zhouyang <zhouyang@hotmail.com>
2019-12-11 01:34:23 +0800

    add model shema and debug api

--- src/api/models/video.py
+++ src/api/models/video.py
@@ -1,13 +1,33 @@
 from api.utils.database import db
+from marshmallow_sqlalchemy import ModelSchema
+from marshmallow import fields
 
 
 class Video(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.string(120), unique=True, nullable=True)
-    image_url = db.Column(db.string(120), unique=True, nullable=True)
-    video_url = db.Column(db.string(120), unique=True, nullable=True)
-    description = db.Column(db.string(120))
+    title = db.Column(db.String(120), unique=True, nullable=True)
+    image_url = db.Column(db.String(120), unique=True, nullable=True)
+    video_url = db.Column(db.String(120), unique=True, nullable=True)
+    description = db.Column(db.String(120))
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+        return self
 
     def __repr__(self):
         return '<Video %r>' % self.title
+
+
+class VideoSchema(ModelSchema):
+
+    class Meta(ModelSchema.Meta):
+        model = Video
+        sqla_session = db.session
+
+    id = fields.Number(dump_only=True)
+    title = fields.String(required=True)
+    image_url = fields.String(required=True)
+    video_url = fields.String(required=True)
+    description = fields.String(required=True)
--- src/api/routes/video.py
+++ src/api/routes/video.py
@@ -1,6 +1,6 @@
-from flask import Blueprint
+from flask import Blueprint, request
 from flask_restful import Api, Resource
-
+from api.models.video import VideoSchema, Video
 
 video_route = Blueprint('video_route', __name__)
 video_api = Api(video_route)
@@ -15,7 +15,18 @@ class VideoItem(Resource):
 class VideoList(Resource):
 
     def get(self):
-        return {'videos': 'video list'}
+        video_schema = VideoSchema()
+        videos = Video.query.all()
+        print(videos)
+        result = video_schema.dumps(videos, many=True)
+        return result, 200
+
+    def post(self):
+        data = request.get_json()
+        video_schema = VideoSchema()
+        video = video_schema.load(data)
+        result = video_schema.dump(video.save())
+        return result, 201
 
 
 video_api.add_resource(VideoItem, '/video/<int:id>')
