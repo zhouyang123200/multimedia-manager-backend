34afa1e20e02b522401912a290a2a5bb74e8ac7a
zhou yang <zhouyang123200@hotmail.com>
2021-01-03 21:59:23 +0800

    add token api

--- src/api/tests/test_user.py
+++ src/api/tests/test_user.py
@@ -1,19 +1,29 @@
 import json
+from api.models import User, UserSchema
+from api.utils.database import db
+
+USERDATA = {
+    'username': 'user1',
+    'passwd': 'admin123',
+    'email': 'user1@test.com'
+}
 
 def test_post_user(app):
     test_uri = '/api/users'
-    data = {
-        'username': 'user1',
-        'passwd': 'admin123',
-        'email': 'user1@test.com'
-    }
-    response = app.test_client().post(test_uri, json=data)
+    response = app.test_client().post(test_uri, json=USERDATA)
     data = json.loads(response.data)
     assert 'user1' == data.get('username')
     assert 'user1@test.com' == data.get('email')
+    assert 'access_token' in data
 
 def test_post_token(app):
-    pass
+    test_uri = '/api/token'
+    user = UserSchema().load(USERDATA)
+    with app.app_context():
+        user.save()
+    responses = app.test_client().post(test_uri, json=USERDATA)
+    data = json.loads(responses.data)
+    assert 'access_token' in data
 
 def test_get_usersporfile(app):
     pass
